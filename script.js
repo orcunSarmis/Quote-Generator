9f3095aff8e9841867614437255109728a3a2c65
Orcun Sarmis <yorcunsarmis@gmail.com>
2023-06-06 21:05:56 +1000

    adding twiter and details

--- script.js
+++ script.js
@@ -1,10 +1,31 @@
+const quoteContainer = document.getElementById('quote-container');
+const quoteText = document.getElementById('quote');
+const authorText = document.getElementById('author');
+const twitterBtn = document.getElementById('twitter');
+const newQuoteBtn = document.getElementById('new-quote');
+
+
 let apiQuotes = [];
 
 // Show new quote
 function newQuote() {
 	// Pick a random quote
 	const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
-	console.log(quote);
+	// if author is missing replace
+	if (!quote.author) {
+		authorText.textContent = 'Unknown';
+	} else {
+		authorText.textContent = quote.author;
+	}
+	// if quote is long
+	if (quote.text.length > 80) {
+		quoteText.classList.add('long-quote');
+	} else {
+		quoteText.classList.remove('long-quote');
+	}
+
+
+	quoteText.textContent = quote.text;
 }
 
 // Get quotes from API
@@ -18,5 +39,16 @@ async function getQuotes() {
 		// Catch err here
 	}
 }
+
+// Tweet quote
+function tweetQuote() {
+	const twitterUrl = `https://twitter.com/intent/tweet?text=${quoteText.textContent} - ${authorText.textContent}`;
+	window.open(twitterUrl, '_blank');
+}
+
+// eventlistner
+newQuoteBtn.addEventListener('click', newQuote);
+twitterBtn.addEventListener('click', tweetQuote);
+
 // On load
 getQuotes();
