2680127b55dffde35cb14b11c58150d08dc091a7
Orcun Sarmis <yorcunsarmis@gmail.com>
2023-06-08 21:08:03 +1000

    code completed

--- script.js
+++ script.js
@@ -3,12 +3,26 @@ const quoteText = document.getElementById('quote');
 const authorText = document.getElementById('author');
 const twitterBtn = document.getElementById('twitter');
 const newQuoteBtn = document.getElementById('new-quote');
+const loader = document.getElementById('loader');
 
 
 let apiQuotes = [];
 
+// Show loading
+function loading() {
+	loader.hidden = false;
+	quoteContainer.hidden = true;
+}
+
+// Hide loading
+function complete() {
+	quoteContainer.hidden = false;
+	loader.hidden = true;
+}
+
 // Show new quote
 function newQuote() {
+	loading();
 	// Pick a random quote
 	const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
 	// if author is missing replace
@@ -24,12 +38,14 @@ function newQuote() {
 		quoteText.classList.remove('long-quote');
 	}
 
-
+	// Set quote hide loader
 	quoteText.textContent = quote.text;
+	complete();
 }
 
 // Get quotes from API
 async function getQuotes() {
+	loading();
 	const apiUrl = 'https://jacintodesign.github.io/quotes-api/data/quotes.json';
 	try {
 		const response = await fetch(apiUrl);
